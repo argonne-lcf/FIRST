0f23f3f1118efcea848af6e1746fd97c3c0eef1e
Ethan Wong <ewong@anl.gov>
2026-05-26 16:04:51 -0500

    clusters/base: use caching api

--- resource_server_async/clusters/cluster.py
+++ resource_server_async/clusters/cluster.py
@@ -5,10 +5,10 @@ from abc import ABC, abstractmethod
 from typing import List, Self, Type
 
 from cachetools import TTLCache
-from django.core.cache import cache
 from django.forms.models import model_to_dict
 
 from inference_gateway.settings import MAINTENANCE_ERROR_NOTICES
+from resource_server_async.cache import get_item_from_cache
 
 from ..auth import check_permission as auth_utils_check_permission
 from ..errors import ClusterNotFound, Unauthorized
@@ -54,7 +54,7 @@ class BaseCluster(ABC):
 
         # Check Redis cache for cluster status from ALCF facility API
         cache_key = f"cluster_status:{self.cluster_name}"
-        cluster_status: ClusterStatus | None = cache.get(cache_key)
+        cluster_status: ClusterStatus | None = get_item_from_cache(cache_key)
 
         if not isinstance(cluster_status, dict):
             cluster_status = {"status": "unknown", "message": ""}
