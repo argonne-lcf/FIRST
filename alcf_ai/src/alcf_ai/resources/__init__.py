ac0277b87e61598fc4d57abbdbfa5ba43fa0464a
Misha Salim <msalim@anl.gov>
2026-07-01 15:01:39 -0500

    Integrate amsc-d3-triton service

--- alcf_ai/src/alcf_ai/resources/__init__.py
+++ alcf_ai/src/alcf_ai/resources/__init__.py
@@ -1,5 +1,6 @@
 from .cluster import ClusterResource
+from .d3_triton import D3TritonResource
 from .resource import ClientResource
 from .sam3 import Sam3Resource
 
-__all__ = ["ClusterResource", "Sam3Resource", "ClientResource"]
+__all__ = ["ClusterResource", "D3TritonResource", "Sam3Resource", "ClientResource"]
--- resource_server_async/schemas/__init__.py
+++ resource_server_async/schemas/__init__.py
@@ -1,9 +1,11 @@
 from .clusters import CheckMaintenanceResult, ClusterStatus
+from .d3_triton import D3TritonRequest
 from .data_transfer import GlobusStagingAreaPrepared
 from .endpoints import ClusterSummary, ListEndpointsResponse
 from .sam3 import Sam3Request
 
 __all__ = [
+    "D3TritonRequest",
     "Sam3Request",
     "ListEndpointsResponse",
     "GlobusStagingAreaPrepared",
