14385d3a383cf84e93d4fd8be83c2929a294742d
BenoitCote <benoitcote@anlvpn108.mcs.anl.gov>
2026-07-23 16:41:28 -0700

    added response_model=None to accomodate StreamingResponse type

--- packages/gateway/first_gateway/apiserver/routes/openai/federated.py
+++ packages/gateway/first_gateway/apiserver/routes/openai/federated.py
@@ -15,21 +15,21 @@ from .dependencies import ChatCompletionsModel, EmbeddingsModel, ResponsesModel
 router = APIRouter(prefix="/federated/v1")
 
 
-@router.post("/chat/completions")
+@router.post("/chat/completions", response_model=None)
 async def chat_completions(
     user: AuthUser, payload: OpenAIChatCompletionsPayload, model: ChatCompletionsModel
 ) -> StreamingResponse | dict[str, Any]:
     raise NotImplementedError("Not implemented yet.")
 
 
-@router.post("/responses")
+@router.post("/responses", response_model=None)
 async def responses(
     user: AuthUser, payload: OpenAIResponsesPayload, model: ResponsesModel
 ) -> StreamingResponse | dict[str, Any]:
     raise NotImplementedError("Not implemented yet.")
 
 
-@router.post("/embeddings")
+@router.post("/embeddings", response_model=None)
 async def embeddings(
     user: AuthUser, payload: OpenAIEmbeddingsPayload, model: EmbeddingsModel
 ) -> StreamingResponse | dict[str, Any]:
