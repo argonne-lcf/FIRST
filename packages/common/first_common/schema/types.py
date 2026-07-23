c900dc6a98244dbc5d942c78098d35189dd92199
Misha Salim <msalim@anl.gov>
2026-07-22 21:11:31 -0500

    update nginx config and fix openapi

--- packages/common/first_common/schema/types.py
+++ packages/common/first_common/schema/types.py
@@ -375,11 +375,8 @@ class SecretRef(str):
     def __get_pydantic_core_schema__(
         cls, source_type: Any, handler: GetCoreSchemaHandler
     ) -> core_schema.CoreSchema:
-        return core_schema.chain_schema(
-            [
-                core_schema.str_schema(),
-                core_schema.no_info_plain_validator_function(cls._validate),
-            ]
+        return core_schema.no_info_after_validator_function(
+            cls._validate, core_schema.str_schema()
         )
 
     @classmethod
