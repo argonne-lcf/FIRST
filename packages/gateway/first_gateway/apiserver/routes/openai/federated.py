90af33ea89cc9cf206339bb75e2266ffa428f216
BenoitCote <benoitcote@anlvpn164.mcs.anl.gov>
2026-07-24 15:57:32 -0700

    added admission controler to API route arguments

--- packages/gateway/first_gateway/apiserver/routes/openai/federated.py
+++ packages/gateway/first_gateway/apiserver/routes/openai/federated.py
@@ -9,7 +9,7 @@ from first_common.schema.endpoints.openai import (
     OpenAIResponsesPayload,
 )
 
-from ...dependencies import AuthUser
+from ...dependencies import AdmissionControllerDep, AuthUser
 from .dependencies import AuthorizedModel
 
 router = APIRouter(prefix="/federated/v1")
@@ -17,20 +17,31 @@ router = APIRouter(prefix="/federated/v1")
 
 @router.post("/chat/completions", response_model=None)
 async def chat_completions(
-    user: AuthUser, payload: OpenAIChatCompletionsPayload, model: AuthorizedModel
+    user: AuthUser,
+    payload: OpenAIChatCompletionsPayload,
+    model: AuthorizedModel,
+    admission_controler: AdmissionControllerDep,
 ) -> StreamingResponse | dict[str, Any]:
+    # backend = await get_backend()
+    # await submit_to_backend()
     raise NotImplementedError("Not implemented yet.")
 
 
 @router.post("/responses", response_model=None)
 async def responses(
-    user: AuthUser, payload: OpenAIResponsesPayload, model: AuthorizedModel
+    user: AuthUser,
+    payload: OpenAIResponsesPayload,
+    model: AuthorizedModel,
+    admission_controler: AdmissionControllerDep,
 ) -> StreamingResponse | dict[str, Any]:
     raise NotImplementedError("Not implemented yet.")
 
 
 @router.post("/embeddings", response_model=None)
 async def embeddings(
-    user: AuthUser, payload: OpenAIEmbeddingsPayload, model: AuthorizedModel
+    user: AuthUser,
+    payload: OpenAIEmbeddingsPayload,
+    model: AuthorizedModel,
+    admission_controler: AdmissionControllerDep,
 ) -> StreamingResponse | dict[str, Any]:
     raise NotImplementedError("Not implemented yet.")
