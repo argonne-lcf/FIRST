f3343acd8b4c88d2d60b87940730460287998dc3
BenoitCote <benoitcote@anlvpn164.mcs.anl.gov>
2026-08-19 14:23:53 -0700

    added submit_inference_with_retry function

--- packages/gateway/first_gateway/services/submit_inference.py
+++ packages/gateway/first_gateway/services/submit_inference.py
@@ -1,11 +1,70 @@
+import logging
 from typing import Any, TypeVar
 
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
-from ..database.redis.router_config import BackendConfig
+from first_gateway.apiserver.dependencies import AuthUser
+
+from ..database.redis.admission import AdmissionController
+from ..database.redis.router_config import BackendConfig, ModelConfig
+from .orchestration import (
+    get_backend,
+    get_backend_candidates,
+    get_deployment_from_backend,
+)
+
+logger = logging.getLogger(__name__)
 
 PayloadT = TypeVar("PayloadT", bound=BaseModel)
+
+
+async def submit_inference_with_retry(
+    user: AuthUser,
+    model: ModelConfig,
+    admission_controller: AdmissionController,
+    payload: PayloadT,
+    deployment_name: str | None = None,
+) -> StreamingResponse | dict[str, Any]:
+    """
+    Send inference requests to targeted backends using the admission
+    controler to select the appropriate backends. This function tries
+    to submit the request and return the response if successful, but
+    fallover to the next most appropriate backends in case of failures.
+    """
+
+    backend_candidates = get_backend_candidates(model, deployment_name=deployment_name)
+
+    for attempt in range(len(backend_candidates)):
+        backend = await get_backend(
+            user,
+            model,
+            admission_controller,
+            backend_candidates,
+        )
+
+        try:
+            response = await submit_inference(backend, payload)
+        except:
+            deployment = get_deployment_from_backend(model.deployments, backend)
+            admission_controller.record_error(
+                backend.id, deployment.router_params
+            )  # count error for cooldown
+            backend_candidates = [
+                b for b in backend_candidates if b.uid != backend.id
+            ]  # don't try this one again
+            token_usage = 0
+            logger.warning(f"Backend {backend.id} failed, trying next one.")
+        else:
+            # TODO: implement parse_token_usage
+            # token_usage = parse_token_usage(response)
+            token_usage = 1  # TEMPORARY
+            return response
+        finally:
+            # TODO: Incorporate request ID
+            admission_controller.settle(
+                "temporary_request_id", actual_tokens=token_usage
+            )
 
 
 async def submit_inference(
