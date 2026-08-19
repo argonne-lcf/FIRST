6b5c3369758e56ea8d05e15333fe9b4dc0a8e4ce
BenoitCote <benoitcote@anlvpn164.mcs.anl.gov>
2026-08-19 14:24:53 -0700

    now using submit_inference_with_retry

--- packages/gateway/first_gateway/apiserver/routes/openai/deployments.py
+++ packages/gateway/first_gateway/apiserver/routes/openai/deployments.py
@@ -9,8 +9,8 @@ from first_common.schema.endpoints.openai import (
     OpenAIResponsesPayload,
 )
 
-from ....services.orchestration import get_backend, get_name_from_slug
-from ....services.submit_inference import submit_inference
+from ....services.orchestration import get_name_from_slug
+from ....services.submit_inference import submit_inference_with_retry
 from ...dependencies import AdmissionControllerDep, AuthUser
 from .dependencies import AuthorizedModel
 
@@ -21,48 +21,48 @@ router = APIRouter(prefix="/deployments/{deployment_slug}/v1")
 async def chat_completions(
     deployment_slug: str,
     user: AuthUser,
-    payload: OpenAIChatCompletionsPayload,
     model: AuthorizedModel,
     admission_controller: AdmissionControllerDep,
+    payload: OpenAIChatCompletionsPayload,
 ) -> StreamingResponse | dict[str, Any]:
-    backend = await get_backend(
+    return await submit_inference_with_retry(
         user,
         model,
         admission_controller,
+        payload,
         deployment_name=get_name_from_slug(deployment_slug),
     )
-    return await submit_inference(backend, payload)
 
 
 @router.post("/responses", response_model=None)
 async def responses(
     deployment_slug: str,
     user: AuthUser,
-    payload: OpenAIResponsesPayload,
     model: AuthorizedModel,
     admission_controller: AdmissionControllerDep,
+    payload: OpenAIResponsesPayload,
 ) -> StreamingResponse | dict[str, Any]:
-    backend = await get_backend(
+    return await submit_inference_with_retry(
         user,
         model,
         admission_controller,
+        payload,
         deployment_name=get_name_from_slug(deployment_slug),
     )
-    return await submit_inference(backend, payload)
 
 
 @router.post("/embeddings", response_model=None)
 async def embeddings(
     deployment_slug: str,
     user: AuthUser,
-    payload: OpenAIEmbeddingsPayload,
     model: AuthorizedModel,
     admission_controller: AdmissionControllerDep,
+    payload: OpenAIEmbeddingsPayload,
 ) -> StreamingResponse | dict[str, Any]:
-    backend = await get_backend(
+    return await submit_inference_with_retry(
         user,
         model,
         admission_controller,
+        payload,
         deployment_name=get_name_from_slug(deployment_slug),
     )
-    return await submit_inference(backend, payload)
